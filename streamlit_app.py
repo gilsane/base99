5df054b9fb548aeef827c675c671b8a4c05de415
gilsane <20244444@sdhs.gwe.hs.kr>
2024-12-06 23:29:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,3 +60,17 @@ st.image("https://i.namu.wiki/i/BQKa8GCZMjCRCDBzbCF7uBtPO_aHoflLZOreUGY7WOlL16Hw
 st.write("유튜브 동영상 예제")
 st.video("https://www.youtube.com/watch?v=aHNsrx8H21g")
 
+
+st.write("Most objects") # df, err, func, keras!
+st.write(["st", "is <", 3])
+st.write_stream(my_generator)
+st.write_stream(my_llm_stream)
+
+st.text("Fixed width text")
+st.markdown("_Markdown_")
+st.latex(r""" e^{i\pi} + 1 = 0 """)
+st.title("My title")
+st.header("My header")
+st.subheader("My sub")
+st.code("for i in range(8): foo()")
+st.html("<p>Hi!</p>")
